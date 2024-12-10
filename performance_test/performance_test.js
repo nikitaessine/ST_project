7d9826178f5ee09727116a1888e2bac2aa469d39
nikitaessine <nikita@MacBook-Pro-4.local>
2024-12-10 16:39:38 +0200

    k6 test

--- performance_test/performance_test.js
+++ performance_test/performance_test.js
@@ -7,17 +7,13 @@ export const options = {
 };
 
 export default function () {
-  // Make a GET request to the target URL
   const res = http.get('http://localhost/register');
 
-  // Check if the response status is 200
   const success = check(res, {
     'status is 200': (res) => res.status === 200,
   });
 
-  // Log the status code
   console.log(`Response status: ${res.status}, Success: ${success}`);
 
-  // Sleep for 1 second to simulate real-world usage
   sleep(1);
 }
